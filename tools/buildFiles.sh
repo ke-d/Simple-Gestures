f2a39939c8f075d9d13719f51e801cc49dca6bc5
mrdokenny <mrdokenny@gmail.com>
2017-05-12 22:59:00 -0700

    Build Chrome

--- tools/buildFiles.sh
+++ tools/buildFiles.sh
@@ -1,10 +1,20 @@
 echo "Building"
 
 DES=builds
-
+rmdir -rf $DES
 mkdir -p $DES
 cd src/
-zip -r test.zip *
-mv test.zip ../$DES/
+
+zip -r Firefox.zip *
+
+
+sed -i '/applications/,+5d' manifest.json
+zip -r Chrome.zip *
+
+
+mv Firefox.zip ../$DES/
+mv Chrome.zip ../$DES/
+
+git checkout manifest.json
 
 echo "Package done."
